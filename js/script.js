380d389e62211a3cfdc555e50afc25b7d67e3536
csrijon <csrijon92@gmail.com>
2024-11-01 22:43:29 +0530

    welcome my github

---
+++ js/script.js
@@ -0,0 +1,81 @@
+new WOW().init();
+
+const chaifood = document.querySelector(".chai-food");
+const pizzafood = document.querySelector(".pizza-food");
+const burgerfood = document.querySelector(".burger-food");
+const allfood = document.querySelector(".all-food");
+const promocards = document.querySelectorAll(".promo-card");
+
+
+// our menu 
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 45) {
+        $('.navbar').addClass('sticky-top shadow-sm');
+    } else {
+        $('.navbar').removeClass('sticky-top shadow-sm');
+    }
+});
+
+
+if (chaifood && promocards) {
+    chaifood.addEventListener("click", (e) => {
+        e.preventDefault();
+        promocards.forEach(card => {
+            card.style.display = "none"
+            card.classList.remove("animate__fadeInUp");
+            if (card.classList.contains("chinese")) {
+                card.style.display = "block"
+                card.classList.add("animate__animated", "animate__fadeInUp"); // Add the animation class
+            };
+        }); 
+    }
+    )
+}else{
+    console.log("Element not found")
+}
+
+if (pizzafood  && promocards) {
+    pizzafood.addEventListener("click",(e) => {
+        e.preventDefault();
+        promocards.forEach(card => {
+            card.style.display = "none"
+            card.classList.remove("animate__fadeInUp");
+            if (card.classList.contains("Pizzaboy")) {
+                card.style.display = "block" 
+                card.classList.add("animate__animated", "animate__fadeInUp");   
+            }           
+        });
+    }
+    )
+}else{
+    console.log("bhai khana nehi hai")
+}
+
+if (burgerfood && promocards ) {
+    burgerfood.addEventListener("click",(e) => {
+        e.preventDefault();
+        promocards.forEach(card => {
+            card.style.display = "none"
+            card.classList.remove("animate__fadeInUp");
+            if (card.classList.contains("burger")) {
+                card.style.display ="block";
+                card.classList.add("animate__animated", "animate__fadeInUp"); 
+        }
+    });
+    })
+}else{
+    console.log("burger nehi hai")
+}
+if (allfood && promocards) {
+    allfood.addEventListener("click",(e) => {
+        e.preventDefault();
+        promocards.forEach(card => {
+            card.style.display ="block"
+            card.classList.add("animate__animated", "animate__fadeInUp"); 
+        });
+    })
+}else{
+    console.log("all food nehi hai")
+}
+// our menu end 
